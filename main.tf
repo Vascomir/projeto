b5ddad41c17cc51862375f1ea701b6bebac39e34
Vascomir <vnetomir@emeal.nttdata.com>
2026-01-29 16:39:48 +0000

    vista

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ terraform {
 
 provider "google" {
   project = "olas-485615"
-  #region  = "europe-west12"
   #credentials = "key.json"
 }
 
@@ -26,14 +25,14 @@ data "google_compute_network" "my-network" {
   name = "vpc-network"
 }
 
-resource "google_compute_global_address" "private_ip_address" {
-  name          = "private-ip-address"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 16
-  network       = data.google_compute_network.my-network.id
+#resource "google_compute_global_address" "private_ip_address" {
+ # name          = "private-ip-address"
+  #purpose       = "VPC_PEERING"
+  #address_type  = "INTERNAL"
+  #prefix_length = 16
+  #network       = data.google_compute_network.my-network.id
 
-}
+#}
 
 resource "google_service_networking_connection" "private_vpc_connection" {
   network                 = data.google_compute_network.my-network.id
